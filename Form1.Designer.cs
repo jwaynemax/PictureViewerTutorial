a7fe6a69f12954cf1a174ef0b8680f580d9b0120
jmaxwellwestga <jm00724@my.westga.edu>
2024-01-12 13:10:26 -0500

    Buttons added to from

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,8 +31,14 @@
             tableLayoutPanel1 = new TableLayoutPanel();
             pictureBox1 = new PictureBox();
             checkBox1 = new CheckBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
             tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            flowLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -42,6 +48,7 @@
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 85F));
             tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
             tableLayoutPanel1.Controls.Add(checkBox1, 0, 1);
+            tableLayoutPanel1.Controls.Add(flowLayoutPanel1, 1, 1);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -74,6 +81,60 @@
             checkBox1.Text = "Stretch";
             checkBox1.UseVisualStyleBackColor = true;
             // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(button1);
+            flowLayoutPanel1.Controls.Add(button2);
+            flowLayoutPanel1.Controls.Add(button3);
+            flowLayoutPanel1.Controls.Add(button4);
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.FlowDirection = FlowDirection.RightToLeft;
+            flowLayoutPanel1.Location = new Point(133, 549);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(733, 55);
+            flowLayoutPanel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.Location = new Point(635, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(95, 25);
+            button1.TabIndex = 0;
+            button1.Text = "Show a picture";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.AutoSize = true;
+            button2.Location = new Point(525, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(104, 25);
+            button2.TabIndex = 1;
+            button2.Text = "Clear the picture";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.AutoSize = true;
+            button3.Location = new Point(369, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(150, 25);
+            button3.TabIndex = 2;
+            button3.Text = "Set the background color";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.AutoSize = true;
+            button4.Location = new Point(288, 3);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 25);
+            button4.TabIndex = 3;
+            button4.Text = "Close";
+            button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -86,6 +147,8 @@
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -94,5 +157,10 @@
         private TableLayoutPanel tableLayoutPanel1;
         private PictureBox pictureBox1;
         private CheckBox checkBox1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -21,5 +21,10 @@ namespace PictureViewer
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
