b81fe187cfed82b4aa6b3ceb8b978a3136cdade7
jmaxwellwestga <jm00724@my.westga.edu>
2024-01-12 13:58:25 -0500

    Added logic for clear, stretch, background color, close. Part 3 of tutorial complete.

--- Form1.cs
+++ Form1.cs
@@ -35,12 +35,16 @@ namespace PictureViewer
 
         private void closeButton_Click(object sender, EventArgs e)
         {
-
+            // Close the form.
+            this.Close();
         }
 
         private void backgroundButton_Click(object sender, EventArgs e)
         {
-
+            // Show the color dialog box. If the user clicks OK, change the
+            // PictureBox control's background to the color the user chose.
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
         }
 
         private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
@@ -50,12 +54,20 @@ namespace PictureViewer
 
         private void stretchCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-
+            // If the user selects the Stretch check box, 
+            // change the PictureBox's
+            // SizeMode property to "Stretch". If the user clears 
+            // the check box, change it to "Normal".
+            if (stretchCheckBox.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
         }
 
         private void clearButton_Click(object sender, EventArgs e)
         {
-
+            // Clear the picture.
+            pictureBox1.Image = null;
         }
     }
 }
